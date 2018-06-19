89f50936b0a7d065950a20a01e1d34c2611cd5c9
Shivam Vats <shivamvats.iitkgp@gmail.com>
2018-06-19 15:44:11 +0530

    Added code to generate random map for planning.
    The obstacle distribution follows a truncated normal distribution.

---
+++ generate_random_map.py
@@ -0,0 +1,45 @@
+import matplotlib.pyplot as plt
+import cv2 as cv
+import numpy as np
+
+from scipy.stats import norm
+import random
+
+def sampleNormal(mean=0, dev=1):
+    randProb = random.random()
+    sample = norm.ppf(randProb)
+    return dev*sample + mean
+
+
+def generateRandomImage(size=(100, 100), mean=1, dev=1):
+    randomMap = np.zeros(shape=size)
+    randomMap.fill(255)
+    for row in range(size[0]):
+        for col in range(size[1]):
+            randNormalVal = sampleNormal(mean, dev)
+            randNormalTruncated = min(randNormalVal, 1)
+            randNormalTruncated = max(randNormalVal, 0)
+            randPixelVal = 255*randNormalTruncated
+            if randPixelVal < 50:
+                randomMap.itemset((row, col), 0)
+
+    return randomMap
+
+
+def displayImage(img):
+    cv.namedWindow('image', cv.WINDOW_NORMAL)
+    cv.imshow('image', img)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+
+def main():
+    randomMap = generateRandomImage(size=(300, 300))
+    displayImage(randomMap)
+    cv.imwrite("rand_normal.png", randomMap)
+
+
+if __name__ == "__main__":
+    main()
+
+
